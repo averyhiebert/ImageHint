0482673949049c238410250dcb216d01d7c87437
Avery Hiebert <averyhiebert@gmail.com>
2021-04-07 00:01:42 -0400

    Switch to using data url, per Mozilla recommendations

--- insert_iframe.js
+++ insert_iframe.js
@@ -31,26 +31,56 @@ function displayResults(query, doc){
     for (i of [0,1,2,3,4]){
         url_args += `&r${i+1}=${encodeURIComponent(results[i])}`
     }
-    insertIFrame(url_args);
+    insertIFrame(query,results);
 }
 
-function insertIFrame(url_args){
-    // TODO: Use data url instead of web-accessible resource, since this
-    //   avoids problems with the web page being able to fingerprint the
-    //   user based on extension ID, and/or problems with the page being
-    //   able to interact with the extension.
-    var url = browser.runtime.getURL("ui/image_list.html");
+function insertIFrame(query,results){
+    /* Create & insert an IFrame containing the resulting images, using
+       a data URL per the recommended security practices from mozilla.org,
+       not that there are any particular obvious security risks either way.*/
 
     // TODO: Remove current iframe if it exists, to prevent multiple elements
     //  with the same id from existing.
+    //  Doesn't seem to be causing any problems currently, though.
 
-    // Add iFrame to page (will contain image results)
+    var parser = new DOMParser();
+    var doc = parser.parseFromString("<html><head></head><body onclick=\"parent.window.postMessage('delete-the-iframe','*');\"></body></html>",
+        "text/html");
+
+    // Create title element
+    var title = doc.createElement("h4");
+    title.innerHTML = query; //TODO escape html characters;
+    title.setAttribute("style","position:fixed;top:0px;left:0px;margin-top:0;padding-top:10px;padding-bottom:10px;background-color:#DDDDDD;width:100%;text-align:center");
+
+    doc.body.appendChild(title)
+    doc.body.appendChild(doc.createElement("br"));
+    doc.body.appendChild(doc.createElement("br"));
+
+    // Insert images
+    var im_list = doc.createElement("div");
+    im_list.setAttribute("style","text-align:center");
+    for (var i = 0; i < 5; i++){
+        var src = results[i];
+        var im = doc.createElement("img");
+        im.src = src
+        im.setAttribute("style","width: 100%");
+        im_list.appendChild(im);
+        im_list.append(doc.createElement("br"));
+        im_list.append(doc.createElement("br"));
+    }
+    doc.body.appendChild(im_list);
+
+    // Convert to text/data url
+    var documentText = (new XMLSerializer()).serializeToString(doc)
+    var dataurl = "data:text/html," + encodeURIComponent(documentText);
+
+    // Insert iframe into main page
     var elem = document.createElement("iframe");
     elem.setAttribute("style", "position:fixed; top:10px; right:10px; background-color:white; border: thin solid black; width: 35%; max-width:300px; height: 300px; z-index:10000000;border-radius:10px;")
-    elem.setAttribute("src",url+url_args);
+    elem.setAttribute("src",dataurl);
     elem.setAttribute("id","inserted-imagehint-iframe")
 
-    var body = document.getElementsByTagName("body")[0]
+    body = document.getElementsByTagName("body")[0]
     body.appendChild(elem);
 }
 
